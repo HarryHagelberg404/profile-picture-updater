436ea96ed68f8423124484a533b48a82fd9ef348
harry hagelberg <harry.hagelberg@fortnox.se>
2023-02-10 16:02:47 +0100

    Functionality now works, waiting for twitter developer access

---
+++ month_bio.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+def months_passed(current_date):
+    current_month = current_date.month
+    months = ['January', 'February', 'March', 'April', 'May', 'June',
+              'July', 'August', 'September', 'October', 'November', 'December']
+    result = ''
+    for i in range(12):
+        if (i + 1) < current_month:
+            result += '|'
+        else:
+            result += '-'
+    return result
+
+# Example usage:
+current_date = datetime.now()
+print(months_passed(current_date))
--- transform.py
+++ transform.py
@@ -1,3 +1,4 @@
+import math
 import PIL
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
@@ -9,20 +10,20 @@ def create_profile_picture(percentage_filled, image_path):
     
     # Resize the inner image to fit inside the circle with a 5px margin
     size = min(inner_image.width, inner_image.height)
-    size = size * 2 - 10
+    size = math.floor(size * 2.4)
     inner_image = inner_image.resize((size, size), Image.ANTIALIAS)
     
     # Create a mask to crop the inner image to a circular shape
     mask = Image.new('L', inner_image.size, 0)
     draw = ImageDraw.Draw(mask)
-    draw.ellipse((0, 0, size, size), fill=255)
+    draw.ellipse((57, 57, size-57, size-57), fill=255)
     
     # Apply the mask to the inner image to make it round
     inner_image = inner_image.copy()
     inner_image.putalpha(mask)
     
     # Create a blank image with a white background
-    image = Image.new('RGB', (500, 500), (255, 255, 255))
+    image = Image.new('RGB', (500, 500), (0,0,0))
     draw = ImageDraw.Draw(image)
     
     # Draw the circle outline
@@ -32,14 +33,14 @@ def create_profile_picture(percentage_filled, image_path):
     fill_angle = 360 * percentage_filled
     
     # Fill the circle with the specified percentage
-    draw.pieslice((0, 0, 500, 500), 90, 90 + fill_angle, fill='black')
+    draw.pieslice((0, 0, 500, 500), 90, 90 + fill_angle, fill='rgba(4,85,254,255)')
     
     # Convert the image to RGBA format
     image = image.convert("RGBA")
     
     # Crop the image to the smallest bounding box that contains the filled circle
     image = image.crop(image.getbbox())
-    
+
     # Calculate the position of the inner image
     x = (500 - size) / 2
     y = (500 - size) / 2
@@ -56,5 +57,5 @@ days_left = (end_of_year - today).days
 percentage_filled = 1 - (days_left / 365)
 
 # Create and save the profile picture
-image = create_profile_picture(percentage_filled, 'inner_image.png')
-image.save('profile_picture.png')
+image = create_profile_picture(percentage_filled, 'bobba.jpg')
+image.save('profile_picture2.png')
